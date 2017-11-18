0b06a3151804f082409ecc3ca0b9a493378e3a6a
AndressaStefany <andressa.stefany@yahoo.com.br>
2017-11-17 22:52:59 -0300

    comentarios

--- teste.cpp
+++ teste.cpp
@@ -11,6 +11,7 @@ int main() {
     Mat image, mask, image_color, second_image, second_image_color, prevT, image_new;
     vector <Point2f> corners, corners_lim[2], great_corners[2];
 
+    // paramentros de calcOpticalFlowPyrLK()
     vector<uchar> status;
     vector<float> err;
     Size winSize(31,31);
@@ -18,11 +19,11 @@ int main() {
 
     double dx=0, dy=0, da=0;
     while(1) {
-        cap >> image;
+        cap >> image;// primeiro frame
         if (image.data == NULL)
             return -1;
 
-        cap >> second_image;
+        cap >> second_image;// segundo frame
         if (second_image.data == NULL)
             return -1;
 
@@ -31,27 +32,32 @@ int main() {
 
         resize(second_image, second_image, Size(600, 500));
         second_image_color = second_image.clone();
-
+        // deixar as imagens em tons de cinza
         cvtColor(image, image, COLOR_BGR2GRAY);
         cvtColor(second_image, second_image, COLOR_BGR2GRAY);
-//    image_cut = image(Rect(200, 150, 200, 200)).clone();
 
+        // achar melhores pontos e borda
+        // metodo Shi-Tomasi
         goodFeaturesToTrack(image, corners, 300, 0.01, 20, mask, 3, false, 0.01);
 
+        // filtra os pontos de borda desejados
         for (int i = 0; i < corners.size(); i++) {
             if (corners[i].x >= 200 && corners[i].y >= 150 && corners[i].x <= 400 && corners[i].y <= 350) {
                 corners_lim[0].push_back(corners[i]);
             }
         }
 
+        // rastrear pontos utilizando metodo Lucas Kanade
         calcOpticalFlowPyrLK(image, second_image, corners_lim[0], corners_lim[1], status, err,
                              winSize, 3, termcrit, 0, 0.001);
 
+        // retira os ponto de bordas ruins
         for(size_t i=0; i < status.size(); i++) {
             if(status[i]) {
                 great_corners[0].push_back(corners_lim[0][i]);
                 great_corners[1].push_back(corners_lim[1][i]);
 
+                // pinta na imagem os pontos
                 circle(image_color, corners_lim[0][i], 3, Scalar(0, 255, 255), -1, 8);
                 circle(second_image_color, corners_lim[1][i], 3, Scalar(0, 255, 255), -1, 8);
             }
@@ -59,6 +65,7 @@ int main() {
 
         imshow("image1", image_color);
 
+        // estima a matriz com o deslocamento em x e y, e o angulo
         Mat T = estimateRigidTransform(great_corners[1], great_corners[0], false);
 
         static double soma_dx=0,soma_dy=0,soma_da=0;
@@ -81,6 +88,7 @@ int main() {
         dy= soma_dy;
         da= soma_da;
 
+        // recalcula a matriz T
         T.at<double>(0,0) = cos(da);
         T.at<double>(0,1) = -sin(da);
         T.at<double>(1,0) = sin(da);
@@ -89,11 +97,12 @@ int main() {
         T.at<double>(0,2) = dx;
         T.at<double>(1,2) = dy;
 
+        // aplica a rotaçao e translado em image_new
         warpAffine(image_color, image_new, T, image_color.size());
 
         imshow("Hyper", image_new);
 
-        //apagar as bordas
+        //apaga as bordas
         corners.clear();
         corners_lim[0].clear();
         corners_lim[1].clear();
