c539f38a663e175d8083e44f02a5aa6995cf7d39
AndressaStefany <andressa.stefany@yahoo.com.br>
2017-11-29 16:10:26 -0300

    relatorio final

---
+++ relatório/code/06.cpp
@@ -0,0 +1,10 @@
+ // sem estabilizacao
+ imshow("Sem", image_color);
+ // com estabilizacao
+ imshow("Com", image_new);
+
+ // limpando
+ greatCorners[0].clear();
+ greatCorners[1].clear();
+ corners[0].clear();
+ corners[1].clear();
